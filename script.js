1a8c6d1b9447cdd51fe4046517e02d781d125f57
Chyna <chynapblair@gmail.com>
2024-01-23 20:00:48 -0500

    Fetch player season stats on search

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const playerDescription = document.getElementById('player-description');
 const playerStats = document.getElementById('player-stats');
 
 // API endpoints
-const playerUrl = 'https://www.balldontlie.io/api/v1/players';
+const playerUrl = 'https://www.balldontlie.io/api/v1/players/';
 const statsUrl = 'https://www.balldontlie.io/api/v1/season_averages';
 
-// Fetch player data
+// Fetch player on search
 const search = () => {
   if (searchBox.value === '' || searchBox.value === ' ') {
     alert('Please enter a player.');
@@ -21,26 +21,27 @@ const search = () => {
         playerDescription.innerHTML = '';
 
         // Store player data from API
-        let playerId = data.data[0].id;
-        let playerName = data.data[0].first_name + ' ' + data.data[0].last_name;
-        let playerPosition =
+        const playerId = data.data[0].id;
+        const playerName =
+          data.data[0].first_name + ' ' + data.data[0].last_name;
+        const playerPosition =
           data.data[0].position === '' ? 'N/A' : data.data[0].position;
-        let playerTeam = data.data[0].team.full_name;
-        let playerHeight =
+        const playerTeam = data.data[0].team.full_name;
+        const playerHeight =
           data.data[0].height_feet === null
             ? 'N/A'
             : data.data[0].height_feet + "'" + data.data[0].height_inches;
-        let playerWeight =
+        const playerWeight =
           data.data[0].weight_pounds === null
             ? 'N/A'
             : data.data[0].weight_pounds + ' lbs';
 
         // Display player information
-        let nameHeader = document.createElement('h1');
+        const nameHeader = document.createElement('h1');
         nameHeader.innerHTML = playerName;
         playerHeader.appendChild(nameHeader);
 
-        let playerInfo = document.createElement('p');
+        const playerInfo = document.createElement('p');
         playerInfo.innerHTML =
           playerTeam +
           ' • ' +
@@ -52,14 +53,27 @@ const search = () => {
         playerDescription.appendChild(playerInfo);
 
         console.log(data.data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
 
-        let errorMsg = document.createElement('p');
-        errorMsg.innerHTML = 'Not found: Please try your search again.';
-        errorMsg.classList.add('error');
-        playerHeader.appendChild(errorMsg);
+        // Fetch player stats from id
+        fetch(statsUrl + `?season=2023&player_ids[]=${playerId}`)
+          .then((response) => response.json())
+          .then((data) => {
+            // Store player stats from API
+            const ppg = data.data[0].pts;
+            const apg = data.data[0].ast;
+            const rpg = data.data[0].reb;
+            const fg = (data.data[0].fg_pct * 100).toFixed(1) + '%';
+
+            console.log(data);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+
+            const errorMsg = document.createElement('p');
+            errorMsg.innerHTML = 'Not found: Please try your search again.';
+            errorMsg.classList.add('error');
+            playerHeader.appendChild(errorMsg);
+          });
       });
     searchBox.value = '';
   }
